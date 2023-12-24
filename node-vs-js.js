bf71119b88d5c06734663bfcda7acd673cd22246
HasanOmar1 <r3domar1@gmail.com>
2023-12-24 11:04:18 +0200

    added

---
+++ node-vs-js.js
@@ -0,0 +1,24 @@
+// Node.js and client-side JavaScript are both based on the JavaScript programming language,
+// but they have some fundamental differences:
+
+// 1 - Environment: Node.js is designed to run JavaScript code on the server-side,
+//     while client-side JavaScript runs in the user's web browser.
+
+// 2 - Capabilities: Client-side JavaScript is limited to what is available in the web browser's JavaScript engine,
+//     while Node.js can access system resources like files and network sockets,
+//     making it suitable for building server-side applications.
+
+// 3 - Modules: Node.js has a built-in module system that allows developers to easily import and export code,
+//     while client-side JavaScript relies on external libraries or frameworks to provide similar functionality.
+
+// 4 - Event-driven programming: Both Node.js and client-side JavaScript use an event-driven programming model,
+//     but the events in client-side JavaScript are typically user-driven (like button clicks or form submissions),
+//     while Node.js events are often related to I/O operations (like reading from a file or making a network request).
+
+// 5 - DOM manipulation: Client-side JavaScript has access to the Document Object Model (DOM) of the web page,
+//     allowing developers to manipulate the content and structure of the page dynamically.
+//     Node.js does not have access to the DOM,
+//     since it runs on the server-side and is not tied to a specific web page.
+
+// Overall, Node.js and client-side JavaScript have different use cases and capabilities,
+// but they both rely on the same fundamental language and programming concepts.
